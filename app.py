aad623165f9376531d3d0afdbd4e4f29afc6fc20
Jéfferson Freitas <139813366+JotaQueiroz@users.noreply.github.com>
2023-09-04 19:57:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, redirect, render_template, request, url_for, jsonify
 
-
+# Chamada pra biblioteca Flask
 app = Flask(__name__)
 
+# Criação da lista "personagens"
 personagens = []
 
+# Direcionamento pra API inicial
 @app.route('/personagem')       
 def inicio():
     return jsonify(personagens)
 
+# Direcionamento pra API de criação de personagens
 @app.route('/personagem/criar', methods = ['POST'])
 def criar():
     data = request.get_json()
@@ -25,5 +28,6 @@ def criar():
     else:
         return jsonify({"message": "Error"}), 400
 
+# Fim do programa
 if __name__ == '__main__':
     app.run( debug = True )
