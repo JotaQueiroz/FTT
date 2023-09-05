cf4d8c9b70dcf3198da54dbb974603c2bce5730a
Jéfferson Freitas <139813366+JotaQueiroz@users.noreply.github.com>
2023-09-05 19:17:10 -0300

    documentação

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 from flask import Flask, redirect, render_template, request, url_for, jsonify
+"""
+    Importação das bibliotecas utilizados no projeto.
+"""
 
-# Chamada pra biblioteca Flask
 app = Flask(__name__)
 
-# Criação da lista "personagens"
 personagens = []
+"""
+    Lista usada para a inclusão dos personagens.
+"""
 
-# Direcionamento pra API inicial
 @app.route('/personagem')       
 def inicio():
+    """
+    Essa função é a pagina de exibição de todos os personagens cadastrados.
+    Return: Retorna a lista de personagens.
+    """
     return jsonify(personagens)
 
-# Direcionamento pra API de criação de personagens
 @app.route('/personagem/criar', methods = ['POST'])
 def criar():
+    """
+    Função utilizada para a crianção dos personagem recebendo os parâmetros:
+    :param name: Nome do personagem
+    :param description: Descrição do personagem
+    :param image: Onde a imagem esta arquivada
+    :param program: Nome do programa do personagem
+    :param animator: Desenhista da arte do personagem
+    Returns: Retorna a mensagem de 'Personagem criado com sucesso!'.
+    """
     data = request.get_json()
     if data:
         personagem = {
@@ -22,7 +37,7 @@ def criar():
             "image": data.get('image'),
             "program": data.get('program'),
             "animator": data.get('animator')
-        }
+            }
         personagens.append(personagem)
         return jsonify({"message": "Personagem criado com sucesso!"}), 201
     else:
