aa74a0236bd89ea91d26c320a0d46249b3cd622c
Jéfferson Freitas <139813366+JotaQueiroz@users.noreply.github.com>
2023-09-12 10:50:01 -0300

    Update LTarefas.py

--- Testes/LTarefas.py
+++ Testes/LTarefas.py
@@ -1,17 +1,21 @@
+# Lista para inclusão de tarefas
 Tarefas = []
 
+# Função para adcionar tarefas a lista
 def adicionar():
     add = input("Qual tarefa deseja adicionar? ")
     Tarefas.append(add)
     print(f"Tarefa:'{add}', adicionado com sucesso!")
-    
+
+# Função para listar as tarefas   
 def listar():
     print("Lista de Tarefas:")
     print("-"*17)
     for i, add in enumerate(Tarefas, start = 1):
         print(f"{i} - {add}")
     print("-"*17)
-        
+
+# Função para excluir as tarefas        
 def excluir():
     listar()
     select = int(input("Escolha o número de qual tarefa deseja excluir: ")) - 1
@@ -21,7 +25,8 @@ def excluir():
         print("Tarefa removida com sucesso!")
     else:
         print("Escolha inexistente!")
-        
+
+# Laço infinito para as opções com 'break' para sair        
 while True:
     print("\nOpções:")
     print("1 - Adicionar tarefa")
